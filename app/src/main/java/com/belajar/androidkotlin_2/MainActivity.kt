1916426e9aa4e2adc3eef7a79313aae457eb0798
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-01-30 06:45:12 +0700

    not now

--- app/src/main/java/com/belajar/androidkotlin_2/MainActivity.kt
+++ app/src/main/java/com/belajar/androidkotlin_2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.belajar.androidkotlin_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
